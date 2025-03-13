faaebe836b3d3ddf77dd267bf89f414a1f757d6b
Ellie Pearson <ellie.jm.pearson@gmail.com>
2025-03-13 14:51:46 -0700

    fix spelling

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def encode_password(password, max_len=16):
     encoded += [0] * (max_len - len(encoded))  # Pad if too short
     return torch.tensor([encoded], dtype=torch.float32)  # Convert to tensor
 
-def predict_strenght(password):
+def predict_strength(password):
     encoded_password = encode_password(password)
     with torch.no_grad():
         output = model(encoded_password)
@@ -33,15 +33,15 @@ if __name__ == '__main__':
     print("2. The program will take the password and run it through our model.")
     print("3. A category will return ranging from 'Very Weak', 'Weak', 'Average', 'Strong', 'Very Strong' signifying how strong your password is.")
     print("4. You can test as many passwords as you like.")
-    print("5. To exit the program enter 'q' when ready.")
+    print("5. Enter 'q' to quit.")
     print("Please be aware that this AI is not 100% accurate.")
-    print("Please follow NIST Password Guidelines or a random password generator to ensure the best security possible.\n")
+    print("Please follow NIST Password Guidelines to ensure the best security possible.\n")
 
 
     while True:
-        password = input("Enter Your Password: ")
-        if password.lower() == "q":
+        submit = input("Enter Your Password: ")
+        if submit.lower() == "q":
             print("Thank you for using this AI-Powered Password Evaluator")
             break
-        strength = predict_strenght(password)
+        strength = predict_strength(submit)
         print("Your password is: {}".format(strength))
